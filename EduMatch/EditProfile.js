fdc3a3a9899cf3456129b7fbd975ba33812df1fb
Gil Halevi <halevigh@gmail.com>
2023-02-18 17:25:53 -0500

    added two inputs

--- EduMatch/EditProfile.js
+++ EduMatch/EditProfile.js
@@ -1,11 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 
+import { TextInput} from 'react-native-paper';
+
+
 import {View, Text } from 'react-native';
+
+
 const EditProfile = (props) => {
     return (    
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Enter Profile</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'left' }}>
+      <View style={{width:300}}>
+        <TextInput label="Name"/>
+        <TextInput label="Age" keyboardType='numeric'/>
+      </View>
       </View>
     );
 }

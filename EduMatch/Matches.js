d82f85c515d27360b0ad73d5072343b0ed87d9c0
ASHLEY HUANG <ach8448@nyu.edu>
2023-02-18 20:22:31 -0500

    matches

--- EduMatch/Matches.js
+++ EduMatch/Matches.js
@@ -1,14 +1,96 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 
-import {View, Text } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Ashley Huang', /*change to name? */
+    expertise: 'Coffee Brewing, Horses, Gardening',
+    interests: 'Coding, Cars, 3D Printing',
+  },
+  {
+    id: '2',
+    title: 'Flora Cai',
+    expertise: 'Gardening, Calculus, Pottery',
+    interests: 'Software development, Calculus, Hedgefunding',
+  },
+  {
+    id: '3',
+    title: 'Krysten Nguyen',
+    expertise: 'Flowers, Visual Aesthetics, Florida ',
+    interests: 'Florida Man, Stocks, Coding'
+  },
+  {
+    id: '4',
+    title: 'Gil Halevi',
+    expertise: 'Hiking, Irrigation, Environmental Awareness',
+    interests: 'Javascript, kdb, Marshall Wace',
+  }
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+// const Item = ({ skills }) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
 const Matches = () => {
-    
-    return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Matches</Text>
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) =>
+            <Card>
+              {/* <View style = {styles.container}> */}
+                  <Text style = {{flex:1,fontSize:15}}>Name: {item.title}{'\n'}</Text>
+                  <Text style = {{flex:1,fontSize:15}}>Expertise: {item.expertise}</Text>
+                  <Text style = {{flex:1,fontSize:15}}>Curiosities: {item.interests}</Text>
+
+
+              {/* </View> */}
+
+            </Card>
+          }
+        keyExtractor={item => item.id} />
+        
+        {/* <Card>
+          <Text style={styles.paragraph}>
+            React Native Card View for Android and IOS using
+            "react-native-paper"
+          </Text>
+        </Card>
+
+        <Card>
+          
+        </Card> */}
       </View>
-    )
-}
- 
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20
+  },
+});
 export default Matches;
+

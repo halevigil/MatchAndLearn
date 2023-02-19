87be5e26f1aa1da3a51922c9915a6b783ac99d6c
Krysten Nguyen <mpn6647@nyu.edu>
2023-02-18 20:44:35 -0500

    edited Profile

--- EduMatch/EditProfile.js
+++ EduMatch/EditProfile.js
@@ -1,53 +1,63 @@
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
-import { TextInput, Menu, Button, Provider } from 'react-native-paper';
+import { TextInput, Menu, Button, Provider, TouchableWithoutFeedback } from 'react-native-paper';
 import { View, Text } from 'react-native';
 
 const EditProfile = (props) => {
 
-  const [menuVisible, setMenuVisible] = React.useState(false);
+  const [teachMenuVisible, setTeachMenuVisible] = React.useState(false);
+  const [learnMenuVisible, setLearnMenuVisible] = React.useState(false);
 
-  const openMenu = () => setMenuVisible(true);
+  const openTeachMenu = () => setTeachMenuVisible(true);
+  const closeTeachMenu = () => setTeachMenuVisible(false);
 
-  const closeMenu = () => setMenuVisible(false);
-
+  const openLearnMenu = () => setLearnMenuVisible(true);
+  const closeLearnMenu = () => setLearnMenuVisible(false);
   return (
     <Provider>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <View style={{ width: 300 }}>
-          <TextInput label="Name" />
+          <TextInput label="Name"/>
           <TextInput label="Age" keyboardType='numeric' />
           <TextInput label="Location" />
 
           <Menu
-            visible={menuVisible}
-            onDismiss={closeMenu}
-            anchor={
-              <TextInput
-                label="Expertise"
-                onFocus={openMenu}
-                onBlur={closeMenu}
-              />
+            visible={openTeachMenu}
+            onDismiss={closeTeachMenu}
+            anchor={<TextInput
+              label="What's your expertises?"
+              onFocus={openTeachMenu}
+              onBlur={closeTeachMenu}
+            />
             }
           >
-            <Menu.Item title="Gardening" onPress={closeMenu} />
-            <Menu.Item title="Web Development" onPress={closeMenu} />
-            <Menu.Item title="Calculus" onPress={closeMenu} />
+            <Menu.Item title="Gardening" onPress={closeTeachMenu} />
+            <Menu.Item title="Web Development" onPress={closeTeachMenu} />
+            <Menu.Item title="Calculus" onPress={closeTeachMenu} />
           </Menu>
+
+
 
           <Menu
-            visible={menuVisible}
-            onDismiss={closeMenu}
-           
-
-            <Menu.Item title="Gardening" onPress={closeMenu} />
-            <Menu.Item title="Web Development" onPress={closeMenu} />
-            <Menu.Item title="Calculus" onPress={closeMenu} />
+            visible={openLearnMenu}
+            onDismiss={closeLearnMenu}
+            anchor={<TextInput
+  
+              label="What are you curious about?"
+              onFocus={openLearnMenu}
+              onBlur={closeLearnMenu}
+            />
+            }
+          >
+            <Menu.Item title="Gardening" onPress={closeLearnMenu} />
+            <Menu.Item title="Web Development" onPress={closeLearnMenu} />
+            <Menu.Item title="Calculus" onPress={closeLearnMenu} />
           </Menu>
 
-          <Button  mode="contained" onPress={() => console.log('Pressed')}>
-          Save
-        </Button>
+
+          <Button mode="contained" onPress={() => console.log('Pressed')}>
+            Save
+          </Button>
         </View>
       </View>
     </Provider>
